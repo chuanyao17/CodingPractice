98e49e073e339b6729430ad99b579f1d784fc935
chuanyao17 <chuanyao17@gmail.com>
2021-10-21 15:46:30 -0400

    update

--- how_many_sentence/how_many_sentence.py
+++ how_many_sentence/how_many_sentence.py
@@ -18,19 +18,20 @@ def countSentence(words, sentences):
             if check_anagram(words[i],words[j]):
                 if words[i] in wordSet:
                     wordSet[words[j]]=wordSet[words[i]]
-                    countSet[wordSet[words[j]]]+=1
                 else:
                     wordSet[words[i]]=numSet
                     wordSet[words[j]]=numSet
-                    countSet[wordSet[words[i]]]=2
                     numSet+=1
+    for word,numSet in wordSet.items():
+        countSet[numSet]=countSet.get(numSet,0)+1
     for w in sentences.split(" "):
         # print(countSet[wordSet[w]])
         if w in wordSet:
             res*=countSet[wordSet[w]]
     return res
-testcase=[(['listen','silent','it','is'],'listen it is silent'),(["ab","ba"],"ab ab")]
+# testcase=[(['listen','silent','it','is'],'listen it is silent'),(["ab","ba"],"ab ab")]
 # testcase=[(['a','b'],'a')]
+testcase=[(['star','tars','stay','tay','seed','dees','eesd','rast','date','ate'],'rast tay star')]
 for words, sentences in testcase:
     # print(words)
     # print(sentences)
