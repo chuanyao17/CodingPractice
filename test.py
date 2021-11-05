ccf4aba7934e39a821cbf09dc8b265c249d26d00
chuanyao17 <chuanyao17@gmail.com>
2021-11-04 22:50:15 -0400

    update

--- test.py
+++ test.py
@@ -15,8 +15,19 @@ from decimal import Decimal, ROUND_HALF_UP
 # print(''.join(test).lower())
 
 
-str_decimal = str(10.025)
-case_d_1 = Decimal(str_decimal).quantize(Decimal(".00"), ROUND_HALF_UP)
-print(f"case decimal 1: {case_d_1}")	# case decimal 1: 0.06
-print(math.floor((case_d_1%1)*100))
-print(math.floor(((72/59)%1)*100))
+# str_decimal = str(10.025)
+# case_d_1 = Decimal(str_decimal).quantize(Decimal(".00"), ROUND_HALF_UP)
+# print(f"case decimal 1: {case_d_1}")	# case decimal 1: 0.06
+# print(math.floor((case_d_1%1)*100))
+# print(math.floor(((72/59)%1)*100))
+
+test=[("g","a"),("b","z"),("a","c"),("a","a"),("b","a"),("z","a")]
+print(test)
+test.sort(key=lambda x:(x[0]))
+print(test)
+test.sort(key=lambda x:(x[1]))
+print(test)
+test.sort(key=lambda x:(x[0],x[1]))
+print(test)
+test.sort(key=lambda x:(x[1],x[0]))
+print(test)
